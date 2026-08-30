fa836841e352ae3ce62db249465ebc9c16381d6b
AlaFadeli <alaeddinfadeli@gmail:com>
2026-08-30 17:01:20 +0100

    fix(process): single-pass resample to eliminate Frankenstein NaN alignment

--- src/fetch/process.py
+++ src/fetch/process.py
@@ -10,15 +10,9 @@ def resample_telemetry(df):
     df['Date'] = pd.to_datetime(df['Date'])
     df = df.sort_values('Date')
     df = df.set_index('Date')
-    df = df.drop(columns=['Time'])
-    continuous = df[['Speed','RPM','X','Y','Throttle','Brake']]
-    discrete = df[['nGear','DRS']]
-    continuous = continuous.infer_objects() 
-    continuous = continuous.resample('100ms').interpolate()
-    discrete = discrete.resample('100ms').ffill()
-    df = pd.concat([continuous,discrete], axis=1)
-
-
+    df = df.drop(columns=['Time'])                   
+    df = df.resample('100ms').interpolate()         
+    df['nGear'] = df['nGear'].ffill()
+    df['DRS']   = df['DRS'].ffill()
     df['SessionTime'] = np.arange(len(df)) * 0.1
-    return df 
-    
+    return df
